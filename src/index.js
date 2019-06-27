5fc8965c96ee436c4fbea86f09779678b1c6e886
Asier Aduriz <aaduriz@vistaprint.com>
2019-06-27 15:58:06 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import styles from './styles.css'
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
 
-export default class ExampleComponent extends Component {
+export default class App extends Component {
   static propTypes = {
     text: PropTypes.string
-  }
+  };
 
   render() {
-    const {
-      text
-    } = this.props
-
+    const searchClient = algoliasearch(
+      "latency",
+      "6be0576ff61c053d5f9a3225e2a90f76"
+    );
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+      <InstantSearch searchClient={searchClient} indexName="demo_ecommerce">
+        <SearchBox />
+        <Hits />
+      </InstantSearch>
+    );
   }
 }
